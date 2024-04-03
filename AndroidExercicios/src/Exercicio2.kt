ca52948f381f825a90283faa5948636224bc469b
Leticia Pinheiro <126022547+LeticiaPinheiroo@users.noreply.github.com>
2024-04-03 17:15:15 -0300

    Add files via upload

---
+++ AndroidExercicios/src/Exercicio2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("I'm \n" +
+            "learning \n" +
+            "kotlin!")
+
+}
+
+// De outra forma ---------
+// println("I'm")
+// println("leaning")
+// println("kotlin!")
